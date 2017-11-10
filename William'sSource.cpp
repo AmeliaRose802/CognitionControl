8ca04c65033b2f8b12fdc9ce99ca4c9fa880860b
AmeliaRose802 <ameliappenrose@gmail.com>
2017-11-10 15:39:42 -0500

    Add files via upload

---
+++ William'sSource.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<cmath>
+#include<ctime>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int checkSpot(int guess[], int answer[]);
+int checkNum(int guess[], int answer[]);
+
+int checkNum(int guess[], int answer[])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+		
+		}
+	}
+}
+
+int checkSpot(int guess[], int answer[])
+{
+	
+}
